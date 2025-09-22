7eac62e9bbc85c201494e0ba817dbe7da58e8228
pe3dru <lionpedro30@gmail.com>
2025-09-22 17:21:40 -0300

    métodos da postagem

---
+++ src/postagem/entities/postagem.entity.ts
@@ -0,0 +1,25 @@
+import { IsNotEmpty } from "class-validator"
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+
+@Entity({name: 'tb_postagens'})
+export class Postagem {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    titulo: string
+
+    @IsNotEmpty()
+    @Column({length: 1000, nullable: false})
+    texto: string
+
+    @UpdateDateColumn()
+    data: Date
+}
+
+//CREATE TABLE tb_postagens()
+// id PRIMARY KEY AUTO_INCREMENT
+// titulo varchar (100) NOT NULL
+// texto varchar (1000) NOT NULL
+// data DATE(6) UPDATED_TIMESTAMP
